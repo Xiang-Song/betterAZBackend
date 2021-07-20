769546175b113a949507f68f9880dd4c7b3b1b37
Xiang Song <himmelsong@gmail.com>
2021-07-20 06:28:14 -0700

    rollback

--- routes/users.js
+++ routes/users.js
@@ -76,14 +76,13 @@ router.post('/replaceBanner',(req, res, next) =>{
       console.log(info.message);
       res.status(401).send(info.message)
     } else {
-      if(req.body.Headline !== '')
-        {await Banner.destroy({
+        await Banner.destroy({
           truncate: true
         });
         let banner = await Banner.create(req.body)
         res.status(201).send(
           banner
-        )}
+        )
     }
   })(req, res, next);
 })
