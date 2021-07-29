45f08b9100ba2c93d60981c1ed7f2494ad450a9b
Xiang Song <himmelsong@gmail.com>
2021-07-29 10:58:24 -0700

    more naming

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
 const Sequelize = require ('sequelize');
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     host: DB_HOST,
-    port: 5432,
+    port: DB_PORT,
     dialect: 'postgres',
     native: true,
     ssl: true
